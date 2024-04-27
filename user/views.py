c2982c680d2371b8876cbbcdf7bfc5524d099923
Manoj <madhikari@power-guard.jp>
2024-04-27 12:02:20 +0900

    adding user register

--- user/views.py
+++ user/views.py
@@ -1,11 +1,23 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm
 
 
 @login_required
 def index(request):
     return render(request, 'user/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'user/register.html', {'form': form})
 
 
 
